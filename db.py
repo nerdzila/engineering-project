812f1f23c2b6adc09cd72c882a8de25db1c68666
Alfonso Sanchez <alfonsos@menta.com.mx>
2019-08-13 00:02:51 -0500

    Backend: first version

---
+++ db.py
@@ -0,0 +1,89 @@
+import hashlib
+import os
+import binascii
+from coord import Location
+from peewee import (
+    DatabaseProxy,
+    Model,
+    AutoField,
+    CharField,
+    FloatField,
+    ForeignKeyField
+)
+
+database_proxy = DatabaseProxy()
+
+
+class InvalidPassword(Exception):
+    pass
+
+
+class BaseModel(Model):
+    class Meta:
+        database = database_proxy
+
+
+class User(BaseModel):
+    id = AutoField(null=False)
+    username = CharField(unique=True, null=False)
+    key = CharField()
+    salt = CharField()
+
+    @classmethod
+    def signup(cls, username, password):
+        salt = os.urandom(32)
+        key = hashlib.pbkdf2_hmac('sha256', password.encode('utf-8'), salt,
+                                  100000)
+        key = binascii.hexlify(key)
+        salt = binascii.hexlify(salt)
+        new_user = cls.create(username=username, key=key, salt=salt)
+
+        return new_user
+
+    @classmethod
+    def authenticate(cls, username, password):
+        user = cls.get(cls.username == username)
+        salt = binascii.unhexlify(user.salt)
+        key = binascii.unhexlify(user.key)
+        test_key = hashlib.pbkdf2_hmac('sha256', password.encode('utf-8'),
+                                       salt, 100000)
+
+        if test_key != key:
+            raise InvalidPassword()
+
+        return user
+
+
+class Car(BaseModel):
+    id = AutoField(null=False)
+    user = ForeignKeyField(User, null=False)
+    license_plate = CharField(unique=True, null=False)
+    latitude = FloatField()
+    longitude = FloatField()
+
+    @classmethod
+    def create_with_user(cls, user: User, license_plate: str,
+                         location: Location) -> 'Car':
+        license_plate = license_plate.strip()
+        if len(license_plate) == 0:
+            raise ValueError("License Plate can't be empty")
+
+        latitude, longitude = location
+        return Car.create(user=user, license_plate=license_plate,
+                          latitude=latitude, longitude=longitude)
+
+    @classmethod
+    def find_user_cars(cls, user: User) -> 'Car':
+        return cls.select().where(Car.user == user)
+
+    @classmethod
+    def find_car_by_plate(cls, user: User, plate: str) -> 'Car':
+        return cls.get(Car.user == user, Car.license_plate == plate)
+
+    def update_location(self, location: Location) -> None:
+        self.latitude = location.latitude
+        self.longitude = location.longitude
+        self.save()
+
+    def location(self) -> Location:
+        return Location(self.latitude, self.longitude)
---
+++ test_backend.py
@@ -0,0 +1,159 @@
+import pytest
+from os import remove
+from db import User, Car, InvalidPassword, database_proxy
+from coord import Location
+from peewee import SqliteDatabase, DoesNotExist, IntegrityError
+
+try:
+    remove('data/test.db')
+except FileNotFoundError:
+    pass
+
+database = SqliteDatabase('data/test.db')
+database_proxy.initialize(database)
+
+with database:
+    database.create_tables([User, Car])
+
+
+user1 = User.signup(
+    username='user1',
+    password='password1'
+)
+
+user2 = User.signup(
+    username='user2',
+    password='password2'
+)
+
+user3 = User.signup(
+    username='user3',
+    password='password3'
+)
+
+
+car1 = Car.create(user=user1, license_plate='LOL1337',
+                  latitude=19.341803, longitude=-99.196494)
+car2 = Car.create(user=user1, license_plate='ASAP123',
+                  latitude=19.341803, longitude=-99.196494)
+car3 = Car.create(user=user1, license_plate="FTW6667",
+                  latitude=19.520504, longitude=-99.109185)
+
+
+@pytest.fixture()
+def default_user():
+    return user1
+
+
+@pytest.fixture()
+def empty_user():
+    return user2
+
+
+@pytest.fixture()
+def mutable_user():
+    return user3
+
+
+@pytest.fixture()
+def default_user_cars():
+    return [car1, car2, car3]
+
+
+@pytest.fixture()
+def default_car():
+    return car1
+
+
+@pytest.fixture()
+def default_location():
+    return Location(19.341803, 99.196494)
+
+
+class TestLocation:
+    def test_create(self):
+        loc = Location.create_from_string('19.456233', '-99.109234')
+        assert loc.latitude == 19.456233
+        assert loc.longitude == -99.109234
+
+    def test_incorrect_format(self):
+        with pytest.raises(ValueError):
+            Location.create_from_string('Hello', 'World')
+
+    def test_incorrect_latitude(self):
+        with pytest.raises(ValueError):
+            Location.create_from_string('-102.345234', '-99.134938')
+
+    def test_incorrect_longitude(self):
+        with pytest.raises(ValueError):
+            Location.create_from_string('-32.345234', '-209.134938')
+
+    def test_iterable(self, default_location):
+        expected_list = [default_location.latitude, default_location.longitude]
+        assert expected_list == [c for c in default_location]
+
+
+class TestUsers:
+    def test_signup(self):
+        new_user = User.signup('user_new', 'password_new')
+        assert new_user.id is not None
+        auth_user = User.authenticate('user_new', 'password_new')
+        assert new_user.id == auth_user.id
+
+    def test_auth_valid(self):
+        auth_user = User.authenticate('user2', 'password2')
+        assert auth_user is not None
+
+    def test_auth_invalid_user(self):
+        with pytest.raises(DoesNotExist):
+            User.authenticate('user4', 'password2')
+
+    def test_auth_invalid_password(self):
+        with pytest.raises(InvalidPassword):
+            User.authenticate('user1', 'badpassword')
+
+
+class TestCars:
+    def test_create(self, mutable_user, default_location):
+        car = Car.create_with_user(mutable_user, 'TEST123', default_location)
+        assert car.id is not None
+
+    def test_create_invalid_plates(self, default_user, default_location):
+        with pytest.raises(ValueError):
+            Car.create_with_user(default_user, '     ', default_location)
+
+    def test_create_duplicate_plates(self, default_user, default_car,
+                                     default_location):
+        with pytest.raises(IntegrityError):
+            Car.create_with_user(default_user, default_car.license_plate,
+                                 default_location)
+
+    def test_find_cars_by_user(self, default_user, default_user_cars):
+        found_ids = set(car.id for car in Car.find_user_cars(default_user))
+        assert set(car.id for car in default_user_cars) == found_ids
+
+    def test_find_car_by_plate(self, default_user, default_car):
+        found_car = Car.find_car_by_plate(default_user,
+                                          default_car.license_plate)
+        assert found_car.id == default_car.id
+
+    def test_car_not_found_for_user(self, default_user, empty_user,
+                                    default_car):
+        with pytest.raises(DoesNotExist):
+            Car.find_car_by_plate(empty_user, default_car.license_plate)
+
+        with pytest.raises(DoesNotExist):
+            Car.find_car_by_plate(default_user, 'MADEUP999')
+
+    def test_update_location(self, default_car, default_location):
+        assert tuple(default_car.location()) != tuple(default_location)
+
+        default_car.update_location(default_location)
+        updated_car = Car.get_by_id(default_car.id)
+        assert tuple(updated_car.location()) == tuple(default_location)
+
+    def test_delete_car(self, mutable_user):
+        car = Car.find_car_by_plate(mutable_user, 'TEST123')
+        num_deleted = car.delete_instance()
+        assert num_deleted == 1
+        assert len(Car.find_user_cars(mutable_user)) == 0
